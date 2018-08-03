936e60f2a5a38130af33ceda7c2251304115cee1
Rishabh Bharti <chap.rishabhk173@gmail.com>
2018-08-03 17:28:35 +0530

    Update comp_template.cpp

--- comp_template.cpp
+++ comp_template.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
+#define ll long long
 #define vi vector<int>
+#define vp vector<pair<int,int>
+#define vvi vector<vi>
 #define pb push_back
 #define mp make_pair
 #define f first
@@ -10,6 +13,7 @@
 #define traced(x) cout<<" "<<#x<<"=["; for(int i=0; i<x.size();i++) cout<<" "<<x[i]<<"("<<i<<")"; cout<<" ]"
 #define fill1d(x,val) memset(x,val,sizeof(x))
 #define fill2d(x,val) fill_n(*x, sizeof(x)/sizeof(**x),val);
+using namespace std;
 //---------------------------------------------------------------------------------------------------------------
 ll m,n,t,A[];
 
